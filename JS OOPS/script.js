357d8a22f44e15e2958f687062c3f0b4bc8feda7
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-01-13 21:09:12 +0530

    Shorting Using-Single-Loop Logic

--- JS OOPS/script.js
+++ JS OOPS/script.js
@@ -18,20 +18,18 @@ function MakePencil(name,color,company,weight,price,unit){
     this.weight = weight;
     this.price = price;
     this.unit = unit;
-  this.use =function(){
+};
+MakePencil.prototype.use =function(){
         h1.style.color = this.color;
   };
-
-};
-let pencil1 = new MakePencil("natraj","black","natraj","100gm","45","10");
+let pencil1 = new MakePencil("natraj","black","natraj","100gm",45,10);
 console.log(pencil1);
 let pencil2 = new MakePencil("doms","red","doms","100gm","45","10");
 console.log(pencil2);
 pencil1.use();
 
 
-
-// log Larg value and small value or Array
+// log Larg value and small value or   short this Array
 
 let data = [10, 4, 1, 99, 34];
 let larg = data[0];
@@ -43,5 +41,22 @@ for(let i=0;i<data.length-1;i++){
   if(data[i]<small){
     small = data[i];
   }
+
 }
 console.log(larg, small);
+
+// shortiing Array
+
+data = [10, 4, 1, 99, 34];
+
+for (let i = 0; i < data.length - 1; i++) {
+  if (data[i] > data[i + 1]) {
+    let temp = data[i];
+    data[i] = data[i + 1];
+    data[i + 1] = temp;
+    i = -1; 
+  }
+}
+
+console.log(data); 
+
