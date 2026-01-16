f9eab14bd993ad54645bee4ee02e0c94dcecec38
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-01-16 22:14:55 +0530

    API POST Request

--- Fatch API  and HTTPS/script.js
+++ Fatch API  and HTTPS/script.js
@@ -9,3 +9,20 @@ fetch("https://randomuser.me/api/")
 .catch((err) =>{
     console.log(err);
 });
+
+
+// POST request
+fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers:{
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+        title:"hello",
+        body: "this is post request",
+        userId:`1`
+    })
+})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err));
